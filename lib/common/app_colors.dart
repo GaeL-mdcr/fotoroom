c56b832d30d216822fd7ea33dac4e2309436d740
GaeL-mdcr <gabrielmallezan@gmail.com>
2026-06-22 15:54:27 -0400

    identidade visual

--- lib/common/app_colors.dart
+++ lib/common/app_colors.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFF069460);
-  static const Color background = Color(0xFFF7F7F7);
-  static const Color textDark = Color(0xFF222222);
+  static const Color primary = Color(0xFF6D28D9);
+  static const Color primaryDark = Color(0xFF2E1065);
+  static const Color primaryLight = Color(0xFFEDE9FE);
+
+  static const Color background = Color(0xFFFAFAFA);
+  static const Color backgroundDark = Color(0xFF0F0F10);
+
+  static const Color surface = Color(0xFFFFFFFF);
+  static const Color surfaceDark = Color(0xFF18181B);
+
+  static const Color textDark = Color(0xFF171717);
+  static const Color textLight = Color(0xFFFFFFFF);
+
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
 }
--- lib/services/facades/pro_image_editor_facade_widget.dart
+++ lib/services/facades/pro_image_editor_facade_widget.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
-import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pro_image_editor/pro_image_editor.dart';
+
+import '../../common/app_colors.dart';
 
 class ProImageEditorFacadeWidget extends StatefulWidget {
   final String imagePath;
@@ -25,6 +27,41 @@ class ProImageEditorFacadeWidget extends StatefulWidget {
 class _ProImageEditorFacadeWidgetState
     extends State<ProImageEditorFacadeWidget> {
   bool _finalizando = false;
+
+  ThemeData _buildEditorTheme(BuildContext context) {
+    final baseTheme = Theme.of(context);
+
+    return baseTheme.copyWith(
+      colorScheme:
+          ColorScheme.fromSeed(
+            seedColor: AppColors.primary,
+            brightness: Brightness.dark,
+          ).copyWith(
+            primary: AppColors.primary,
+            primaryContainer: AppColors.primaryDark,
+            onPrimary: AppColors.white,
+            surface: AppColors.surfaceDark,
+            onSurface: AppColors.white,
+          ),
+      scaffoldBackgroundColor: AppColors.black,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: AppColors.primaryDark,
+        foregroundColor: AppColors.white,
+      ),
+      iconTheme: const IconThemeData(color: AppColors.white),
+      sliderTheme: baseTheme.sliderTheme.copyWith(
+        activeTrackColor: AppColors.primaryLight,
+        inactiveTrackColor: AppColors.surfaceDark,
+        thumbColor: AppColors.primaryLight,
+        overlayColor: AppColors.primaryLight.withValues(alpha: 0.18),
+      ),
+      bottomSheetTheme: const BottomSheetThemeData(
+        backgroundColor: AppColors.primaryDark,
+        modalBackgroundColor: AppColors.primaryDark,
+        surfaceTintColor: Colors.transparent,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,9 +88,9 @@ class _ProImageEditorFacadeWidgetState
         },
       ),
       configs: ProImageEditorConfigs(
-        theme: Theme.of(context),
+        theme: _buildEditorTheme(context),
         mainEditor: const MainEditorConfigs(
-          enableCloseButton: false,
+          enableCloseButton: true,
           enableZoom: true,
           enableDoubleTapZoom: true,
           enableSubEditorPage: true,
@@ -66,6 +103,96 @@ class _ProImageEditorFacadeWidgetState
             SubEditorMode.blur,
             SubEditorMode.emoji,
           ],
+          style: MainEditorStyle(
+            background: AppColors.black,
+            appBarBackground: AppColors.primaryDark,
+            appBarColor: AppColors.white,
+            bottomBarBackground: AppColors.primaryDark,
+            bottomBarColor: AppColors.white,
+            uiOverlayStyle: SystemUiOverlayStyle.light,
+          ),
+        ),
+        paintEditor: const PaintEditorConfigs(
+          style: PaintEditorStyle(
+            background: AppColors.black,
+            appBarBackground: AppColors.primaryDark,
+            appBarColor: AppColors.white,
+            bottomBarBackground: AppColors.primaryDark,
+            bottomBarActiveItemColor: AppColors.primaryLight,
+            bottomBarInactiveItemColor: AppColors.white,
+            initialColor: AppColors.primary,
+            editSheetBackgroundColor: AppColors.primaryDark,
+            editSheetColor: AppColors.white,
+            editSheetPreviewAreaColor: AppColors.black,
+            uiOverlayStyle: SystemUiOverlayStyle.light,
+          ),
+        ),
+        textEditor: const TextEditorConfigs(
+          style: TextEditorStyle(
+            background: Color(0xCC000000),
+            appBarBackground: AppColors.primaryDark,
+            appBarColor: AppColors.white,
+            bottomBarBackground: AppColors.primaryDark,
+            inputCursorColor: AppColors.primaryLight,
+            inputHintColor: AppColors.primaryLight,
+            fontScaleBottomSheetBackground: AppColors.primaryDark,
+          ),
+        ),
+        cropRotateEditor: const CropRotateEditorConfigs(
+          style: CropRotateEditorStyle(
+            background: AppColors.black,
+            appBarBackground: AppColors.primaryDark,
+            appBarColor: AppColors.white,
+            bottomBarBackground: AppColors.primaryDark,
+            bottomBarColor: AppColors.white,
+            cropCornerColor: AppColors.primaryLight,
+            helperLineColor: AppColors.primaryLight,
+            aspectRatioSheetBackgroundColor: AppColors.primaryDark,
+            aspectRatioSheetForegroundColor: AppColors.white,
+            uiOverlayStyle: SystemUiOverlayStyle.light,
+          ),
+        ),
+        tuneEditor: const TuneEditorConfigs(
+          style: TuneEditorStyle(
+            background: AppColors.black,
+            appBarBackground: AppColors.primaryDark,
+            appBarColor: AppColors.white,
+            bottomBarBackground: AppColors.primaryDark,
+            bottomBarActiveItemColor: AppColors.primaryLight,
+            bottomBarInactiveItemColor: AppColors.white,
+            uiOverlayStyle: SystemUiOverlayStyle.light,
+          ),
+        ),
+        filterEditor: const FilterEditorConfigs(
+          style: FilterEditorStyle(
+            background: AppColors.black,
+            appBarBackground: AppColors.primaryDark,
+            appBarColor: AppColors.white,
+            previewTextColor: AppColors.white,
+            previewSelectedTextColor: AppColors.primaryLight,
+            uiOverlayStyle: SystemUiOverlayStyle.light,
+          ),
+        ),
+        blurEditor: const BlurEditorConfigs(
+          style: BlurEditorStyle(
+            background: AppColors.black,
+            appBarBackgroundColor: AppColors.primaryDark,
+            appBarForegroundColor: AppColors.white,
+            uiOverlayStyle: SystemUiOverlayStyle.light,
+          ),
+        ),
+        layerInteraction: const LayerInteractionConfigs(
+          style: LayerInteractionStyle(
+            borderColor: AppColors.primaryLight,
+            dragSelectionBackground: Color(0x336D28D9),
+            dragSelectionBorderColor: AppColors.primaryLight,
+            buttonRemoveBackground: AppColors.primaryDark,
+            buttonRemoveColor: AppColors.white,
+            buttonEditTextBackground: AppColors.primaryDark,
+            buttonEditTextColor: AppColors.white,
+            buttonScaleRotateBackground: AppColors.primaryDark,
+            buttonScaleRotateColor: AppColors.white,
+          ),
         ),
       ),
     );
