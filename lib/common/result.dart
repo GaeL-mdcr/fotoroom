eed52054ef70f59e816011024ddf9caa31e06ff2
GaeL-mdcr <gabrielmallezan@gmail.com>
2026-06-22 16:30:52 -0400

    limpar arquivos mortos e fortalecer tipagem

--- lib/common/result.dart
+++ lib/common/result.dart
@@ -4,8 +4,23 @@ class Result<T> {
 
   const Result.success(this.data) : error = null;
 
-  const Result.failure(this.error) : data = null;
+  const Result.failure(String message) : data = null, error = message;
 
   bool get isSuccess => error == null;
+
   bool get isFailure => error != null;
+
+  T get dataOrThrow {
+    final value = data;
+
+    if (value == null) {
+      throw StateError('Resultado não possui dados.');
+    }
+
+    return value;
+  }
+
+  String get errorOrDefault {
+    return error ?? 'Erro desconhecido.';
+  }
 }
